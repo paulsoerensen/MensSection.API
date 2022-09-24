0789c1b68d46562755eac68b25ada0eeeb2859f5
Paul Sørensen <paul.soerensen@gmail.com>
2022-09-24 19:23:22 +0200

    db

---
+++ Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace MensSection.Api.Models
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ Models/UserValidate.cs
@@ -0,0 +1,35 @@
+﻿namespace MensSection.Api.Models
+{
+    public class UserValidate
+    {
+        //This method is used to check the user credentials
+        public static bool Login(string username, string password)
+        {
+            var UserLists = GetUsers();
+            return UserLists.Any(user =>
+                user.UserName.Equals(username, StringComparison.OrdinalIgnoreCase)
+                && user.Password == password);
+        }
+
+        public static List<User> GetUsers()
+        {
+            // In Real-time you need to get the data from any persistent storage
+            // For Simplicity of this demo and to keep focus on Basic Authentication
+            // Here we are hardcoded the data
+            List<User> userList = new List<User>();
+            userList.Add(new User()
+            {
+                ID = 101,
+                UserName = "MaleUser",
+                Password = "123456"
+            });
+            userList.Add(new User()
+            {
+                ID = 101,
+                UserName = "FemaleUser",
+                Password = "abcdef"
+            });
+            return userList;
+        }
+    }
+}
