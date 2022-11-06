247907f1d078779033b89f570de40295e7ce2f17
paulsoerensen <paul.soerensen@gmail.com>
2022-11-06 18:49:15 +0100

    slet

--- src/MensSection.Api/EndpointDefinitions/AdminEndpoint.cs
+++ src/MensSection.Api/EndpointDefinitions/AdminEndpoint.cs
@@ -44,8 +44,8 @@ public class AdminEndpoint : IEndpointDefinition
             //return ctx.Response.WriteAsync(config);
             //_logger.LogInformation(_config.GetConnectionString("SqlDbConnectionString"));
             _logger.LogInformation(s);
-            s = System.Configuration.ConfigurationManager.ConnectionStrings["SqlDbConnectionString"]?.ConnectionString;
-            _logger.LogInformation($"From ConfigurationManager:{s}");
+            //s = System.Configuration.ConfigurationManager.ConnectionStrings["SqlDbConnectionString"]?.ConnectionString;
+            //_logger.LogInformation($"From ConfigurationManager:{s}");
             s = _config["ConnectionStrings:SqlDbConnectionString"];
             _logger.LogInformation($"From Config:{s}");
 
